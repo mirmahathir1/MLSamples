d4b1d70d14baa9292b623d5aa2ab59ba3a0bf454
Mahathir <mirmahathir1@gmail.com>
2020-08-05 23:19:45 +0600

    Cleaned up pychache and ipython checkpoint

--- upload.sh
+++ upload.sh
@@ -1,6 +1,9 @@
+
 echo "Enter commit message: "
 read -r message
 echo "The commit message is: $message"
+find ./ -name ".ipynb_checkpoints" -exec rm -R {} \;
+find ./ -name "__pycache__" -exec rm -R {} \;
 git add .
 git commit -am "$message"
 git push origin master
